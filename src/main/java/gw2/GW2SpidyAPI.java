2ef94af1fef83a827c5adc8e3798a8fce30888dc
Kallen Tu <kallentu@hotmail.ca>
2019-04-02 21:44:01 -0700

    GW2SpidyParser with All Items 1 Silver Range Dataset (#2)
    * Initial parser for one dataset, writes to input.txt file.
    
    * Added rounding for the dataset.

--- src/main/java/gw2/GW2SpidyAPI.java
+++ src/main/java/gw2/GW2SpidyAPI.java
@@ -1,3 +1,5 @@
+package gw2;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
@@ -5,51 +7,53 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
-public class GW2Spidy {
+public class GW2SpidyAPI {
     private static final String GW2_SPIDY_URL = "http://www.gw2spidy.com/api/v0.9/json/";
     private static final String RESULTS_KEY = "results";
+    private static final String RESULT_KEY = "result";
 
     /**
      * Gets list of item types and subtypes.
      * Eg. Armor -> Gloves
      */
-    public static JSONObject getTypesJSON() throws IOException {
-        return sendRequest("types");
+    public static JSONArray getTypesJSON() throws IOException {
+        return sendRequest("types").getJSONArray(RESULTS_KEY);
     }
 
     /**
      * Gets list of crafting disciplines.
      * Eg. Weaponsmithing
      */
-    public static JSONObject getDisciplinesJSON() throws IOException {
-        return sendRequest("disciplines");
+    public static JSONArray getDisciplinesJSON() throws IOException {
+        return sendRequest("disciplines").getJSONArray(RESULTS_KEY);
     }
 
     /**
      * Gets list of item rarities.
      * Eg. Rare, Exotic
      */
-    public static JSONObject getRaritiesJSON() throws IOException {
-        return sendRequest("rarities");
+    public static JSONArray getRaritiesJSON() throws IOException {
+        return sendRequest("rarities").getJSONArray(RESULTS_KEY);
     }
 
     /**
      * Gets list of all items.
      */
-    public static JSONObject getAllItemsJSON() throws IOException {
-        return sendRequest("all-items/all");
+    public static JSONArray getAllItemsJSON() throws IOException {
+        return sendRequest("all-items/all").getJSONArray(RESULTS_KEY);
     }
 
     /**
      * Gets list of all items of a certain type.
      * Eg. All gloves which is typeid of 1
      */
-    public static JSONObject getItemsOfTypeJSON(int typeId) throws IOException {
-        return sendRequest("all-items/" + typeId);
+    public static JSONArray getItemsOfTypeJSON(int typeId) throws IOException {
+        return sendRequest("all-items/" + typeId).getJSONArray(RESULTS_KEY);
     }
 
     /**
@@ -74,7 +78,7 @@ public class GW2Spidy {
      * }
      */
     public static JSONObject getItemDataJSON(int itemId) throws IOException {
-        return sendRequest("item/" + itemId);
+        return sendRequest("item/" + itemId).getJSONObject(RESULT_KEY);
     }
 
     /**
@@ -124,18 +128,18 @@ public class GW2Spidy {
     }
 
     /** Gets related items by name. Might be slow, not recommended. */
-    public static JSONObject getItemsOfNameJSON(String name) throws IOException {
-        return sendRequest("item-search/" + name);
+    public static JSONArray getItemsOfNameJSON(String name) throws IOException {
+        return sendRequest("item-search/" + name).getJSONArray(RESULTS_KEY);
     }
 
     /** Get a list of all crafting recipes. */
-    public static JSONObject getAllRecipesJSON() throws IOException {
-        return sendRequest("recipes/all");
+    public static JSONArray getAllRecipesJSON() throws IOException {
+        return sendRequest("recipes/all").getJSONArray(RESULTS_KEY);
     }
 
     /** Gets a list of all crafting recipes for a certain discipline. */
-    public static JSONObject getRecipesOfDisciplineJSON(int disciplineId) throws IOException {
-        return sendRequest("recipes/" + disciplineId);
+    public static JSONArray getRecipesOfDisciplineJSON(int disciplineId) throws IOException {
+        return sendRequest("recipes/" + disciplineId).getJSONArray(RESULTS_KEY);
     }
 
     /**
@@ -156,18 +160,18 @@ public class GW2Spidy {
      * }
      */
     public static JSONObject getRecipeDataJSON(int recipeId) throws IOException {
-        return sendRequest("recipes/" + recipeId);
+        return sendRequest("recipes/" + recipeId).getJSONObject(RESULT_KEY);
     }
 
     /** Get the current gem/gold conversion rate. */
     public static JSONObject getGemPriceJSON() throws IOException {
-        return sendRequest("gem-price");
+        return sendRequest("gem-price").getJSONObject(RESULT_KEY);
     }
 
     private static JSONObject sendRequest(String arguments) throws IOException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet(GW2_SPIDY_URL + arguments);
-            System.out.println("Executing GW2Spidy request " + httpGet.getRequestLine());
+            System.out.println("Executing gw2.GW2SpidyAPI request " + httpGet.getRequestLine());
 
             // Returns the response as a String if successful, for further parse into JSON object
             ResponseHandler<String> responseHandler = response -> {
@@ -184,7 +188,7 @@ public class GW2Spidy {
             System.out.println("----------------------------------------");
             System.out.println(responseBody);
 
-            return new JSONObject(responseBody).getJSONObject(RESULTS_KEY);
+            return new JSONObject(responseBody);
         }
 
     }
---
+++ src/main/java/gw2/GW2SpidyParser.java
@@ -0,0 +1,42 @@
+package gw2;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GW2SpidyParser {
+    public static void allItemsThresholdOneSilverToFile() throws IOException {
+        // Clear input file ready for writing
+        Path file = Paths.get("input.txt");
+        Files.deleteIfExists(file);
+        Files.createFile(file);
+
+        List<String> fileLines = new ArrayList<>();
+
+        // Retrieve all items to round up offer and round down selling.
+        // This will cause some error with calculation, but can be treated as rounding as we will earn more.
+        JSONArray allItemsJson = GW2SpidyAPI.getAllItemsJSON();
+        for (int i = 0; i < allItemsJson.length(); i++) {
+            JSONObject item = allItemsJson.getJSONObject(i);
+
+            // Round up to nearest 100
+            int maxOfferUnitPrice = (item.getInt("max_offer_unit_price") + 100) / 100 * 100;
+            // Round down to nearest 100
+            int minSaleUnitPrice = (item.getInt("min_sale_unit_price") - 100) / 100 * 100;
+
+            String itemLine = maxOfferUnitPrice + "\t" +
+                    minSaleUnitPrice + "\t" +
+                    item.getInt("offer_availability") + "\t" +
+                    item.getInt("sale_availability") + "\n";
+            fileLines.add(itemLine);
+        }
+
+        Files.write(file, fileLines);
+    }
+}
