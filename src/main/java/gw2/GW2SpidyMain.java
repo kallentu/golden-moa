c1a850f7709e06030caf61212963e20f69cbe4fe
kallentu <kallentu@hotmail.ca>
2019-04-14 01:56:25 -0400

    Fixed parser and added (temporary) main.

--- src/main/java/gw2/GW2SpidyMain.java
+++ src/main/java/gw2/GW2SpidyMain.java
@@ -2,10 +2,10 @@ package gw2;
 
 import java.io.IOException;
 
-public class GW2Main {
+public class GW2SpidyMain {
     public static void main(String[] args) {
         try {
-            GW2SpidyParser.allItemsToFile();
+            GW2SpidyParser.addAllItemsToFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
