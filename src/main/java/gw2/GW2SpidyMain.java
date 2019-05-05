f37fd5a38453e000c72d233511ea41743f2a45d3
kallentu <kallentu@hotmail.ca>
2019-05-05 00:42:17 -0400

    Abstracted out the rounding threshold for parser and dataset main.

--- src/main/java/gw2/GW2SpidyMain.java
+++ src/main/java/gw2/GW2SpidyMain.java
@@ -2,12 +2,23 @@ package gw2;
 
 import java.io.IOException;
 
+/** Generates GW2 trading post dataset from GW2Spidy. */
 public class GW2SpidyMain {
-    public static void main(String[] args) {
-        try {
-            GW2SpidyParser.addAllItemsToFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    /** Usage: hadoop jar JAR gw2.GW2SpidyMain [--threshold THRESHOLD] */
+    public static void main(String[] args) throws IOException{
+        GW2SpidyParser gw2SpidyParser = new GW2SpidyParser();
+
+        // Additional optional arguments.
+        // [--threshold THRESHOLD]  default: 100
+        if (args.length != 0) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+                if (arg.equals("--threshold")) {
+                    gw2SpidyParser.setRoundingBound(Integer.valueOf(args[++i]));
+                }
+            }
         }
+        gw2SpidyParser.createAllItemsDataSetFile();
     }
 }
